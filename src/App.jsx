e1a11a9633c6ad93d9ef1ddb7c84cc3f96f9f605
Jeremy <jeremy.teurterie@gmail.com>
2023-08-11 08:40:45 +0700

    :sparkles: Conditionals

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,26 @@ function App() {
     { id: 3, text: 'Comment three' },
   ];
 
+  const loading = false;
+  const showComments = true;
+
+  if (loading) return <h1>Loading...</h1>;
+
   return (
     <div className="container">
       <h1>{title.toUpperCase}</h1>
       <p>{body}</p>
 
-      <div className="comments">
-        <h3>Comments ({comments.length})</h3>
-        <ul>
-          {comments.map((comment, index) => (
-            <li key={index}>{comment.text}</li>
-          ))}
-        </ul>
-      </div>
+      {showComments && (
+        <div className="comments">
+          <h3>Comments ({comments.length})</h3>
+          <ul>
+            {comments.map((comment, index) => (
+              <li key={index}>{comment.text}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
