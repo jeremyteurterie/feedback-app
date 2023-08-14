3ce548c0805708da021cfac989d80d5b48def301
Jeremy <jeremy.teurterie@gmail.com>
2023-08-14 10:53:30 +0700

    :sparkles: Update feedback item

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -39,6 +39,13 @@ export const FeedbackProvider = ({ children }) => {
       setFeedback(feedback.filter((item) => item.id !== id));
   };
 
+  // Update feedback item
+  const updateFeedback = (id, updItem) => {
+    setFeedback(
+      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+    );
+  };
+
   // Set iten to be updated
   const editFeedback = (item) => {
     setFeedbackEdit({
@@ -51,10 +58,11 @@ export const FeedbackProvider = ({ children }) => {
     <FeedbackContext.Provider
       value={{
         feedback,
+        feedbackEdit,
         deleteFeedback,
         addFeedback,
         editFeedback,
-        feedbackEdit,
+        updateFeedback,
       }}
     >
       {children}
