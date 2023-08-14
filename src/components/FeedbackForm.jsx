b5d6aa6714db59f84a3265d9980a3c2c02745657
Jeremy <jeremy.teurterie@gmail.com>
2023-08-14 08:49:25 +0700

    :sparkles: Rating selection component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import Card from './shared/Card';
 import Button from './shared/Button';
+import RatingSelect from './RatingSelect';
 
 function FeedbackForm() {
   const [text, setText] = useState('');
+  const [rating, setRating] = useState('');
   const [btnDisabled, setBtnDisabled] = useState(true);
   const [message, setMessage] = useState('');
 
@@ -26,6 +28,7 @@ function FeedbackForm() {
     <Card>
       <form>
         <h2>How would you rate your service with us?</h2>
+        <RatingSelect select={(rating) => setRating(rating)} />
         <div className="input-group">
           <input
             onChange={handleTextChange}
@@ -42,4 +45,5 @@ function FeedbackForm() {
     </Card>
   );
 }
+
 export default FeedbackForm;
